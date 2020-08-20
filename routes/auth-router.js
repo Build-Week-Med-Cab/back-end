3e6a258cc2bd4d49033e9487ae10579c8278af30
Adam-Young <awy6448@gmail.com>
2020-08-19 23:40:35 -0400

    changed object being sent to db.

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -12,7 +12,8 @@ router.post('/register', valadateUserCreation, userAlreadyExists, async (req, re
     const hash = bcrypt.hashSync(req.validUser.password, 12)
     req.validUser.password = hash
     console.log(req.validUser)
-    const newUser = await Users.add(req.validUser)
+    const user = req.validUser
+    const newUser = await Users.add(user)
     const token = genarateToken(newUser)
     res.status(201).json({message: `Welcome ${newUser.username}`, token})
   }catch(error){
